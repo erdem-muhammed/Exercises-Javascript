2421011611d886cf39fe6506f9d4b882fa2c460c
muhammed erdem <erdem.muhammed1@outlook.com>
2021-05-31 09:39:30 +0200

    js part has been added

--- exercise 3/exercise3.js
+++ exercise 3/exercise3.js
@@ -1,11 +1,76 @@
-var nächste = document.getElementById("forward");
-var letzte = document.getElementById("back");
+var image1 = document.getElementById("image1");
+var image2 = document.getElementById("image2");
+var image3 = document.getElementById("image3");
+
+var text1 = document.getElementById("text1");
+var text2 = document.getElementById("text2");
+var text3 = document.getElementById("text3");
+
+
+var slide = 1;
 
 function next()
 {
-    if(nächste)
+  slide++;   
+  
+  
+    if(slide == 2)
     {
-      document.getElementById("image1").style.opacity = "0";
-      document.getElementById("image2").style.opacity = "1";
+      image1.style.opacity = "0";
+      image2.style.opacity = "100%";
+
+      text1.style.opacity = "0";
+      text2.style.opacity = "100%";
     }
+    else if(slide == 3)
+    {
+      image2.style.opacity = "0";
+      image3.style.opacity = "100%";
+
+      text2.style.opacity = "0";
+      text3.style.opacity = "100%";
+    }
+    else
+    {
+      image3.style.opacity = "0";
+      image1.style.opacity = "100%";
+
+      text3.style.opacity = "0";
+      text1.style.opacity = "100%";
+      slide = 1;
+    }
+ 
+}
+
+function prev()
+{
+  slide--;
+
+    if(slide == 0)
+     {
+       image1.style.opacity = "0";
+       image3.style.opacity = "100%";
+
+       text1.style.opacity = "0";
+       text3.style.opacity = "100%";
+
+       slide = 3;
+     }
+     else if(slide == 1)
+     {
+       image2.style.opacity = "0";
+       image1.style.opacity = "100%";
+
+       text2.style.opacity = "0";
+       text1.style.opacity = "100%";
+     }
+     else if(slide == 2)
+     {
+       image3.style.opacity = "0";
+       image2.style.opacity = "100%";
+
+       text3.style.opacity = "0";
+       text2.style.opacity = "100%";
+     }
+    
 }
