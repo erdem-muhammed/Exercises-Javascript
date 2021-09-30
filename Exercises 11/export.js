6e1d415354c7e7128028587e9eb2f723f6db1a7a
muhammed erdem <erdem.muhammed1@outlook.com>
2021-09-30 22:56:41 +0200

    import { x, y, z }

--- Exercises 11/export.js
+++ Exercises 11/export.js
@@ -8,3 +8,9 @@ export
 {
     addition
 }
+
+
+const x = 5;
+const y = 8;
+const z = 10;
+export{ x, y, z}
--- Exercises 11/script.js
+++ Exercises 11/script.js
@@ -3,3 +3,6 @@
 import { addition } from "./export.js";
 console.log(addition);
 console.log(addition(5, 7));
+
+import { x, y, z} from "./export.js";
+console.log(x, y, z);
