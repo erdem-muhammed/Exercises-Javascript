3cf700eba6aa2df3469b6579c3a4d3a56970bbc5
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-19 12:06:34 +0200

    const ccreated

---
+++ Exercise 14- Cookies/script.js
@@ -0,0 +1,4 @@
+const loginForm = document.getElementById("loginForm");
+const profil = document.getElementById("userProfile");
+const us = document.getElementById("userName");
+const pw = document.getElementById("password");
