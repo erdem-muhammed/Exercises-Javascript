068197f1c741d802983e14c9683462df8acc4542
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-19 12:12:09 +0200

    function login() created

--- Exercise 14- Cookies/script.js
+++ Exercise 14- Cookies/script.js
@@ -1,4 +1,13 @@
 const loginForm = document.getElementById("loginForm");
-const profil = document.getElementById("userProfile");
+const profile = document.getElementById("userProfile");
 const us = document.getElementById("userName");
 const pw = document.getElementById("password");
+
+function login()
+{
+    if(us.value == "admin" && pw.value == "123456")
+    {
+        profile.style.display = "block";
+        loginForm.style.display = "none";
+    }
+}
